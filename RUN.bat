50ee8d9181a2564a3700bcd10cf6da2ec0e370eb
AttiPlayz <a.hatzistamatis@gmail.com>
2022-02-14 19:26:43 +1100

    Update RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
 
-title AttiPlayz Premade Server 1.18.1 Build 5
+title AttiPlayz Premade Server 1.18.1 Build 6
 
 echo Starting Paper
  
 java -Xmx1G -jar paper.jar nogui
 PAUSE
+echo ---------------------------------
+echo Thank you for choosing AttiPlayz!
+echo ---------------------------------
